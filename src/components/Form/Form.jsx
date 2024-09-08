e29f0dc1f9796c3ceaf31e757204e6b54639fe54
xivni1337 <sasahomic79@gmail.com>
2024-09-08 10:49:50 +0300

    add components

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Form.css"
+
+const Form = () => {
+    return (
+        <div className="form">
+            form
+        </div>
+    )
+}
+export default Form;
---
+++ src/components/Productlist/Productlist.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Productlist.css"
+
+const Productlist = (props) => {
+    return (
+        <div className="productlist">
+            Productlist
+        </div>
+    )
+};
+
+export default Productlist;
