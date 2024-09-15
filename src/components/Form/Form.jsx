9954ac13ff9c95fbde3bd5c3387348bbad3db32b
xivni1337 <sasahomic79@gmail.com>
2024-09-15 10:17:12 +0300

    form comlite

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,9 +2,31 @@ import React from "react";
 import "./Form.css"
 
 const Form = () => {
+    const [city, setCity] = React.useState("");
+    const [country, setCountry] = React.useState("");
+    const [subject, setSubject] = React.useState("physical");
+
+    const onChangeCountry = (e) => {
+        setCountry(e.target.value);
+    }
+
+    const onChangeCity = (e) => {
+        setCity(e.target.value);
+    }
+
+    const onChangeSubjects = (e) => {
+        setSubject(e.target.value);
+    }
+
     return (
-        <div className="form">
-            form
+        <div className={"form"}>
+            <h1>Ввидите ваши данные</h1>
+            <input type="text" className={"input"} value={country} onChange={onChangeCountry} placeholder={"Страна"}/>
+            <input type="text" className={"input"} value={city} onChange={onChangeCity} placeholder={"оГорот"}/>
+            <select className={"select"} value={subject} onChange={onChangeSubjects}>
+                <option value={"physical"}>ФизЛИцооо</option>
+                <option value={"legal"}>ЮрЛИцооо</option>
+            </select>
         </div>
     )
 }
