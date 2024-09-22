ea608c715f33c6fb5b8b56e220f4083176b8559e
xivni1337 <sasahomic79@gmail.com>
2024-09-22 10:28:03 +0300

    debug commit

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import Header from "./components/Header/Header";
 import {Route, Routes} from "react-router-dom"
 import Productlist from "./components/Productlist/Productlist";
 import Form from "./components/Form/Form";
+import {useEffect} from "react";
 
 function App() {
-    const {onToggleButton} = useTg()
+    const {tg} = useTg()
+    useEffect(()=>{
+        tg.ready()
+    },[])
     return (
     <div className="app">
         <Header></Header>
